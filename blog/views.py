acf1760c59ee9e1cce84dbefd254cb5138dbaaa3
Matthew Hughes <matt.h17@yahoo.com>
2024-02-09 19:53:31 +0000

    added docstrings

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,18 @@ from .forms import CommentForm
 
 # Create views here 
 class PostList(generic.ListView):
+    """
+    Returns all published posts in :model:`blog.Post`
+    and displays them in a page of six posts. 
+    **Context**
+    ``queryset``
+        All published instances of :model:`blog.Post`
+    ``paginate_by``
+        Number of posts per page.
+        
+    **Template:**
+    :template:`blog/index.html`
+    """
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
